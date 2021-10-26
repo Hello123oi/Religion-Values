3484f1be6c97936999a1d03afa9ffc707980ff10
Oiboioi <87256455+Hello123oi@users.noreply.github.com>
2021-10-27 10:42:04 +1100

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,253 +1,253 @@
 questions = 
     [[//Catholic`
-        [//4
+        [//5
             "I favour a Catholic Theocracy as a form of government.",
         ],
-        [//3
+        [//4
             "The Great Schism and its consequences have been a disaster for the human race.",
             "I would not support a leader who is not Catholic.",
         ],
-        [//2
+        [//3
             "Mary is the mother of God.",
             "The only way to reverse a marriage is to have it annulled by the Catholic Church.",
         ],
-        [//1
+        [//2
             "If a woman wants a position in the Church, she may become a nun.",
         ],
-        [//-1
+        [//-2
             "The Pope's authority is illegitimate.",
             "People do not need to attend Catholic Mass every week.",
         ],
-        [//-2
+        [//-3
             "The fall of the Papal States was a good thing.",
         ],
-        [//-3
+        [//-4
             "Purgatory is not a real place.",
         ],
-        [//-4
+        [//-5
             "Jesus was not the founder of the Catholic Church.",
             "Adherents to Catholicism are evil.",
         ]],[//Protestant`
-        [//4
+        [//5
             "I favour Protestant Theocracy as a form of government.",
             "Martin Luther was a noble man opressed by a corrupt and evil authority.",
         ],
-        [//3
+        [//4
             "Salvation is by faith, grace and Christ alone.",
         ],
-        [//2
+        [//3
             "All believers should be able to attain priesthood or an equivalent position.",
             "I believe in the Doctrine of Sola Scripture.",
         ],
-        [//1
+        [//2
             "Works do not contribute to salvation.",
         ],
-        [//-1
+        [//-2
             "Protestantism misinterprets much of the Bible.",
         ],
-        [//-2
+        [//-3
             "First and Second Maccabees should be accepted as part of the Bible.",
             "Protestant Evangelism is dangerous.",
         ],
-        [//-3
+        [//-4
             "WASPs use their religion to opress minorities.",
             "Modern Protestantism is only nominally Christian.",
         ],
-        [//-4
+        [//-5
             "Adherents to Protestant Christianity are evil.",
         ]],[//Orthodox
-		[//4
+		[//5
             "I favour Orthodox Christian Theocracy as a form of government.",
             "Gregory Palamas is a legitimate Saint and his view of God is correct.",
             "The essence of God trascends the energies of God to an infinite degree.",
         ],
-        [//3
+        [//4
             "God is triune.",
         ],
-        [//2
+        [//3
             "The Filioque is heresy.",
             "The Bishop of Rome having jurisdiction over the entire church is a major obstacle to unity between East and West.",
         ],
-        [//1
+        [//2
              "Faith without works is dead.",
 	],
-        [//-1
+        [//-2
             "Hesychasm is heretical.",
             "God is etirely immutable.",
         ],
-        [//-2
+        [//-3
             "I have significant theological issues with Eastern Orthodoxy.",
             "The authority of the Eastern Patriarchs is legitimate.",
         ],
-        [//-3
-        ],
         [//-4
+        ],
+        [//-5
             "Members of the Eastern Orthodox Church are evil.",
         ]],[//Jewish
-		[//4
+		[//5
             "God's covenant with the Jews is still in affect.",
         ],
-        [//3
+        [//4
             "I favour a Jewish Theocracy as a form of government.",
         ],
-        [//2
+        [//3
             "Israel's rightfull land spans from the Nile to the Euphraties.",
         ],
-        [//1
+        [//2
             "Uncircumcised people go to hell.",
             "The modern Jews are God's chosen people.",
             "The Talmud is equal to the Torah/Old Testament.",
         ],
-        [//-1
-        ],
         [//-2
         ],
         [//-3
+        ],
+        [//-4
             "Modern Judaism is ethnonationalism posing as a religion.",
             "Jews are the Synagogue of Satan.",
             "The Jews are inherently evil and seek to destroy everything that is not Jewish.",
         ],
-        [//-4
+        [//-5
             "The Jews killed Jesus.",
             "God willed the destruction of the Second Temple.",
             "The Jews have deserved everything that has been done to them in the past 3500 years.",
         ]],[//Islam
-		[//4
+		[//5
             "Jesus is meerly a prophet.",
         ],
-        [//3
+        [//4
             "Jerusalem should be a Muslim city.",
         ],
-        [//2
+        [//3
             "Mecca and Medina are holy cities.",
         ],
-        [//1
+        [//2
             "The kaaba is to be venerated.",
             "Islam is not an inherently violent religion.",
             "Sharia should govern the entire world.",
         ],
-        [//-1
-        ],
         [//-2
         ],
         [//-3
-            "Islam is a heretical sect of Catholicism.",
+        ],
+        [//-4
+		"Islam is a heretical sect of Catholicism.",
             "Muslims are savages who are unwilling to civilise.",
             "Cleansing the Earth of Islam should be a priority for the West.",
         ],
-        [//-4
+        [//-5
             "Muhammed is not a legitimate prophet.",
             "Poligamy should be strictly forbidden.",
             "Sharia law is unjust.",
         ]],[//Buddhist
-		[//4
+		[//5
             "Desire is the cause of suffering.",
         ],
-        [//3
+        [//4
             "Religious people must be open minded.",
         ],
-        [//2
+        [//3
             "Reincartion is real.",
         ],
-        [//1
+        [//2
             "Dualistic thought is harmful.",
             "Buddhist theocracy is an ideal form of government.",
             "All who do not attain oneness with all things are unenlightened.",
         ],
-        [//-1
-        ],
         [//-2
         ],
         [//-3
+        ],
+        [//-4
             "Buddhism is a self-centered religion.",
             "Buddhism is ridiculous and illogical.",
             "Karma is not real.",
         ],
-        [//-4
+        [//-5
             "The doctrines of Buddhism are shrouded in vaguery because they nonsensical.",
             "Life is not meerly suffering.",
             "Buddhists are evil.",
         ]],[//Pagan
-		[//4
+		[//5
             "The concept of monotheism is false.",
         ],
-        [//3
+        [//4
             "Animals have an aspect of divinity",
         ],
-        [//2
+        [//3
             "Abrahamic Religions and their consequences have been a disaster for the human race.",
         ],
-        [//1
+        [//2
             "Nature is divine and should be worshipped.",
             "Monotheism must be violently opposed.",
             "Christianity, Judaism and Islam are weak and can easily be defeated by superior pagan religions.",
         ],
-        [//-1
-        ],
         [//-2
         ],
         [//-3
+        ],
+        [//-4
             "The extermination of pagans is necessary.",
             "Pagan idols should be burned.",
             "There is one God.",
         ],
-        [//-4
+        [//-5
             "Nature worship is wrong.",
             "Animals don't deserve rights.",
             "Christianity destroyed the Roman Empire.",
         ]],[//Satanist
-		[//4
+		[//5
             "The story of the Garden of Eden portrays Satan as a hero.",
         ],
-        [//3
+        [//4
             "No god should have authority over humanity.",
         ],
-        [//2
+        [//3
             "The morality of the Biblical devil is better than the morality of the Biblical God.",
         ],
-        [//1
+        [//2
             "I truly believe that Satan is real and that he is morally righteous.",
             "Abrahamic religions must be violently opposed.",
             "Most, if not all Satanists are good people.",
         ],
-        [//-1
-        ],
         [//-2
         ],
         [//-3
+        ],
+        [//-4
             "Satan is the embodyment of all that is evil and unjust.",
             "Satanic idols should be burned.",
             "The worship of Satan is bad.",
         ],
-        [//-4
+        [//-5
             "Satanists should be excluded from public positions.",
             "Satanism should not be treated equally to other religions.",
             "Satanists are bad people.",
         ]],[//Religiosity
-		[//4
+		[//5
             "I would prefer to live under the laws of other religions than under the laws of an atheist",
         ],
-        [//3
+        [//4
             "So-called atheists are often the most supersticious people.",
         ],
-        [//2
+        [//3
             "Even agnosticism is deplorable and unacceptable.",
         ],
-        [//1
+        [//2
             "There is real wisdom contained within the doctrines of many religions that atheists ignorantly reject.",
             "I favour theocracy of some kind.",
             "I hate atheists.",
         ],
-        [//-1
-        ],
         [//-2
         ],
         [//-3
+        ],
+        [//-4
             "Society has progressed beyond the need for religion.",
             "Religious people are ignorant and their beliefs are antiquated.",
             "Religion should be illegal.",
         ],
-        [//-4
+        [//-5
             "There is no such thing as objective morality",
             "The Bible is fiction.",
             "God is not real.",
